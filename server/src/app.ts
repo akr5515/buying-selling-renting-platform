1f97fb1e11b2da2fcaac53a124a543a8ff4f1b09
akr5515 <akrangkon5515@gmail.com>
2023-09-19 01:08:30 +0600

    feature: graphql setup.

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,11 @@ import express from "express";
 const app = express();
 import prisma from "./config/database";
 
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import cors from "cors";
+
 // const port = 3000;
 
 app.get("/", (req, res) => {
@@ -10,6 +15,29 @@ app.get("/", (req, res) => {
 
 async function main() {
   await prisma.$connect();
+
+  const server = new ApolloServer({
+    typeDefs: `
+        type User {
+            name: String!            
+        }
+        type Query {
+            getUsers: [User]
+        }
+    `,
+    resolvers: {
+      Query: {
+        getUsers: () => [{ name: "Angkon" }],
+      },
+    },
+  });
+
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
   app.listen(process.env.PORT ? process.env.PORT : 8000, () => {
     console.log(
       `Application is running on ${process.env.PORT ? process.env.PORT : 8000}`
