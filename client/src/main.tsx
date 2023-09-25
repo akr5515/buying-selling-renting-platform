5fa17d564404548b5087659876720ec9dbdd65af
akr5515 <akrangkon5515@gmail.com>
2023-09-25 23:07:13 +0600

    doc: Added documentation for environment setup.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql/v1",
+  uri: process.env.SERVER_URL,
   cache: new InMemoryCache(),
 });
 
